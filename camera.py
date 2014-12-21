d24a6ca8d704bb9849109707ff4cc7480b0c4ac6
Eric Chio <im.ckieric@gmail.com>
2014-12-21 08:32:22 +0800

    Initial commit.

---
+++ camera.py
@@ -0,0 +1,22 @@
+import cv2
+
+class VideoCamera(object):
+    def __init__(self):
+        # Using OpenCV to capture from device 0. If you have trouble capturing
+        # from a webcam, comment the line below out and use a video file
+        # instead.
+        self.video = cv2.VideoCapture(0)
+        # If you decide to use video.mp4, you must have this file in the folder
+        # as the main.py.
+        # self.video = cv2.VideoCapture('video.mp4')
+    
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        success, image = self.video.read()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#
+# Project: Video Streaming with Flask
+# Author: Log0 <im [dot] ckieric [at] gmail [dot] com>
+# Date: 2014/12/21
+# Description:
+# Modified to support streaming out with webcams, and not just raw JPEGs.
+# Most of the code credits to Miguel Grinberg.
+# Original: http://blog.miguelgrinberg.com/post/video-streaming-with-flask
+#
+# Usage:
+# 1. Run "python main.py".
+# 2. navigate the browser to the local webpage.
+from flask import Flask, render_template, Response
+from camera import VideoCamera
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
